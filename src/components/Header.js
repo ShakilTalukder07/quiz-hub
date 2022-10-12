645a26f6ba4665163b6215a32a7fc03ab21db60b
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-12 21:05:44 +0600

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="bg-gray-100 grid grid-cols-1 lg:grid-cols-2">
                 <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
                     <h1 className="text-xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
-                        Web Quiz
+                        Quiz Hub
                     </h1>
                     <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">
                         <span className='text-blue-600'>You are Welcome </span>
